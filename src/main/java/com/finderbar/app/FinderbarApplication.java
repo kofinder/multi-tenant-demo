4f28fc1eec8a31a16a40c438273625c3a223fcce
theinlwin <theinlwin@blue-stone.com>
2019-05-09 17:11:07 +0630

    [add file]

---
+++ src/main/java/com/finderbar/app/FinderbarApplication.java
@@ -0,0 +1,13 @@
+package com.finderbar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderbarApplication.class, args);
+	}
+
+}
